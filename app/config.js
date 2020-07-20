dcc1d22f98de36b71a766d58aab839c84999957f
nicotrozzo <rtrozzo@itba.edu.ar>
2020-07-20 20:10:47 -0300

    Added app config.js

--- app/config.js
+++ app/config.js
@@ -1,3 +1,7 @@
 export const config = {
+<<<<<<< 18acb19bf18919b78ab33ff0a6cf9ad08235b62e
     address: 'localhost:8080'
+=======
+    address: '127.0.0.1:8080'
+>>>>>>> Added app config.js
 };
--- server/index.js
+++ server/index.js
@@ -50,12 +50,20 @@ let app;
 
     // Serve client files
     app.use(express.static(process.cwd() + "/app/dist/app/"));
+<<<<<<< 18acb19bf18919b78ab33ff0a6cf9ad08235b62e
     
+=======
+
+>>>>>>> Added app config.js
     // Serve index.html for default route
     app.get('*', (req, res) => {
         res.sendFile(process.cwd() + "/app/dist/app/index.html");
     });
 
+<<<<<<< 18acb19bf18919b78ab33ff0a6cf9ad08235b62e
+=======
+
+>>>>>>> Added app config.js
     // Console message
     console.log(`[Server] The server is listening to port ${config.server.port}`);
 })();
