79da79e96c78351111a8f1f0512206dee5a7eca7
kammann123 <lucaskammann@hotmail.com>
2020-07-21 04:35:44 -0300

    ResonanceAPI added...

--- app/src/app/components/room/room.component.ts
+++ app/src/app/components/room/room.component.ts
@@ -45,7 +45,6 @@ export class RoomComponent implements OnInit {
     private route: ActivatedRoute,
     private session: SessionService,
     private meeting: MeetingService,
-    private spatial: SpatialService,
     private snackbar: MatSnackBar,
     private clipboard: Clipboard
     //private streamNode: MediaStreamAudioDestinationNode
@@ -326,18 +325,10 @@ export class RoomComponent implements OnInit {
     if ( await this.meeting.getClient().roomExists(roomName) ) {
       // Setting the local Attendee instance, and getting the media device
       // streaming instances, for both audio and video
-      this.local = new Attendee(
-        user.email,
-        {
-          context: this.context,
-          hrir: this.hrir,
-          brir: this.brir
-        }
-      );
+      this.local = new Attendee(user.email);
       let videoStream = await window.navigator.mediaDevices.getUserMedia({ video: true });
       let audioStream = await window.navigator.mediaDevices.getUserMedia({ audio: true });
       this.local.addStream(MediaStreamTypes.WebCam, videoStream);
-      this.local.addStream(MediaStreamTypes.Microphone, audioStream);
       this.local.setMicrophoneStatus(true);
 
       // Setting the Meeting Client
